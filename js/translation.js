42645167b7bbcc8cbe2a8d0a1ddbfed00dacb539
nathandb7 <nathandb7@gmail.com>
2024-02-21 17:34:13 -0300

    Seleccion de idioma y Traduccion con js

---
+++ js/translation.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Guarda el HTML original al cargar la página
+    let originalHTML;
+
+    // Función para restablecer al estado original
+    function resetToOriginal() {
+        document.removeEventListener('change', changeLanguage);
+        document.documentElement.innerHTML = originalHTML;
+        document.getElementById('language').addEventListener('change', changeLanguage);
+    }
+
+    // Lógica para cambiar los textos y contenido según el idioma seleccionado
+    function changeLanguage() {
+        let selectedLanguage = document.getElementById('language').value;
+
+        if (selectedLanguage === 'es') {
+            // Si es español, restablece al estado original
+            resetToOriginal();
+        } else if (selectedLanguage === 'en') {
+            // Guarda el HTML original si aún no se ha guardado
+            if (!originalHTML) {
+                originalHTML = document.documentElement.innerHTML;
+            }
+
+            // Cambiar textos al inglés
+            document.querySelector('.a1').textContent = 'About Me';
+            document.querySelector('.a2').textContent = 'Projects';
+            document.querySelector('.a3').textContent = 'Education';
+            document.querySelector('.a4').textContent = 'Contact';
+
+            document.querySelector('.about-title').textContent = 'About Me';
+            document.querySelector('.about-description').textContent = 'I am a passionate Web Developer with outstanding skills in HTML, CSS, JavaScript, React and PHP. My main focus is on creating captivating and functional user experiences. I have contributed to the success of projects through engaging frontend design and efficient backend implementation using PHP and MySQL. ';
+            document.querySelector('.about-description-2').textContent = 'With a proactive attitude and a constant commitment to continuous improvement, I am ready to face challenges and provide innovative solutions. My ability to adapt to new technologies and my focus on keeping up to date with the latest trends position me as a versatile and results-oriented professional.';
+            document.querySelector('.projects-title').textContent = 'Featured Projects';
+
+            let [p1, p2, p3, p4, p5, p6, p7, p8] = document.querySelectorAll('.project p');
+            p1.textContent = 'Project with React, Vite, Tailwind CSS, LocalStorage';
+            p2.textContent = 'Project with React, Vite, Styled Components, Custom Hooks';
+            p3.textContent = 'Project with HTML, SASS and Gulp';
+            p4.textContent = 'Project with React, Vite, LocalStorage';
+            p5.textContent = 'Real estate site HTML, SASS, Gulp, PHP, MySql';
+            p6.textContent = 'Project with HTML, CSS';
+            p7.textContent = 'Wordpress project';
+            p8.textContent = 'Wordpress project';
+
+            // Selecciona los elementos h3 dentro de la clase .project
+            let [h3_1, h3_2, h3_3, h3_4, h3_5, h3_6, h3_7, h3_8] = document.querySelectorAll('.project h3');
+
+            // Establece el contenido de los elementos h3 a una cadena vacía
+            h3_1.textContent = 'App - Veterinary Patient Monitoring';
+            h3_2.textContent = 'App - Cryptocurrency Quote';
+            h3_3.textContent = 'Web - Event promotion';
+            h3_4.textContent = 'App - Expense Control';
+            h3_5.textContent = 'Website - Real Estate';
+            h3_6.textContent = 'Website - Frond End Store';
+            h3_7.textContent = 'Website - Nimbus';
+            h3_8.textContent = 'Website - Neozix';
+
+            // Selecciona los enlaces dentro de la clase .project
+            let enlacesCodigo = document.querySelectorAll('.project a');
+
+            // Traduce el texto de los enlaces
+            enlacesCodigo.forEach(enlace => {
+                if (enlace.textContent === 'Ver Codigo') {
+                    enlace.textContent = 'View Code';
+                } else if (enlace.textContent === 'Ver Proyecto') {
+                    enlace.textContent = 'View Project';
+                }
+            });
+
+
+            document.querySelector('.educacion h2').textContent = 'Education'
+            document.querySelector('.contact h2').textContent = 'Contact'
+
+            document.querySelector('footer p').textContent = '© 2024 Nathan de Barros. All rights reserved.'
+        }
+    }
+
+    // Agrega un evento al cambio de idioma
+    document.getElementById('language').addEventListener('change', changeLanguage);
+});
